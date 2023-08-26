7f1404a273d54a3d57cafd6a8c8b07eb3d1aac4b
bhishaksanyal <sanyalbishak93@gmail.com>
2023-08-26 20:33:57 +0530

    chore: initial commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Bhishak Sanyal on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
