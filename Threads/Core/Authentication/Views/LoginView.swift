2a4b63b7e97f262fd552ae0050d6a3f70b5ed4ad
bhishaksanyal <sanyalbishak93@gmail.com>
2023-08-27 12:52:10 +0530

    feat: Added loginView UI

--- Threads/Core/Authentication/Views/LoginView.swift
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -8,8 +8,82 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image("threads_app_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(15)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal, 20)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(15)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal, 20)
+                }
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 20)
+                        .foregroundColor(.black)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width - 40, height: 50)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Sign Up")
+                } label: {
+                    HStack (spacing: 3) {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+
+
+            }
+        }
     }
 }
 
