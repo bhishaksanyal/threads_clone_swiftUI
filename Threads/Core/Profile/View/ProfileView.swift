fc026535b6b4c6ee4f077c8c1816b72018a63297
bhishaksanyal <sanyalbishak93@gmail.com>
2023-09-24 18:55:01 +0530

    feat: profile added

--- Threads/Core/Profile/View/ProfileView.swift
+++ Threads/Core/Profile/View/ProfileView.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var filterBarWidth: CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 20
+    }
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(spacing: 30) {
@@ -48,7 +56,39 @@ struct ProfileView: View {
                 })
                 
                 // User content list view
-                
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases) { filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == filter ? .bold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundColor(.black)
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .matchedGeometryEffect(id: "item", in: animation)
+                                } else {
+                                    Rectangle()
+                                        .foregroundColor(.clear)
+                                        .frame(width: filterBarWidth, height: 1)
+                                }
+                            }
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    selectedFilter = filter
+                                }
+                            }
+                        }
+                    }
+                    
+                    LazyVStack {
+                        ForEach(0...10, id: \.self) { thread in
+                            ThreadCell()
+                        }
+                    }
+                }
                 
             }
         }
